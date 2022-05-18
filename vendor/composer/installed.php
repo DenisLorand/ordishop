e41e23c0322a4620d753de8abe7d0eefd05163b2
DenisLorand <73839528+DenisLorand@users.noreply.github.com>
2022-05-18 23:38:13 +0200

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'kercode/mvc-2',
+  ),
+  'versions' => 
+  array (
+    'kercode/mvc-2' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
