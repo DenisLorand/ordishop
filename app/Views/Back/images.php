aa9cc44250a43fc49c148b3acf18bce2c40b11a1
DenisLorand <73839528+DenisLorand@users.noreply.github.com>
2022-05-26 23:40:04 +0200

    Add files via upload

--- app/Views/Back/images.php
+++ app/Views/Back/images.php
@@ -1,5 +1,14 @@
 <?php ob_start(); ?>
 
+<?php
+  // On initialise la session d'identification administration
+  session_start();
+  // On vérifie si l'administrateur est connecté, sinon on le redirige vers la page de connexion
+  if(!isset($_SESSION["mail"]) && !isset ($_SESSION["pass"])){
+    header("Location: indexAdmin.php");
+    exit(); 
+  }
+?>
 
 <article class="createPresta">
     <h4><a class="createPresta" href="indexAdmin.php?action=image">Ajouter une image de présentation</a></h4>
